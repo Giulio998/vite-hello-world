a3389240168a3e287c620af2d74596fc726c8b5d
Giulio998 <giulio_favia@hotmail.it>
2024-04-09 14:14:06 +0200

    Completed + Bonus

---
+++ vite-project/src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<template>
+<div id="hero">
+    <h1>“TITOLO!”</h1>
+</div>    
+
+</template>
+
+<style scoped>
+#hero{
+    background-image: url("../assets/mountain.jpeg");
+    background-size: cover;
+    height: calc(100vh - 80px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+h1{
+    font-size: 40px;
+}
+
+</style>
---
+++ vite-project/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+<div id="title">
+    <h1>“La mia prima app con Vite!”</h1>
+</div>
+
+</template>
+
+<style scoped>
+h1{
+    text-align: center;
+    font-size: 40px;
+}
+#title{
+    height: 80px;
+}
+</style>
